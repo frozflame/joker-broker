5cfeb7f6a908fe55a1afb4a2e1a2f90c4de1b621
Hailong <lendoli@163.com>
2017-09-07 08:37:55 +0800

    RB at top level: joker.broker.ResourceBroker

--- joker/broker/__init__.py
+++ joker/broker/__init__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+from joker.broker.access import ResourceBroker
+
 
 __version__ = '0.0.14'
 
 
 def get_resource_broker(path=None):
-    from joker.broker.access import ResourceBroker
     from joker.broker.default import default_conf_path
     return ResourceBroker.create(path or default_conf_path)
-
-
