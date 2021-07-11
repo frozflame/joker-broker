a27a2ee8e8c6df5798f6589d5294c42b849de8b3
frozflame <lendoli@163.com>
2021-07-11 09:52:34 +0800

    0. GlobalInterface  1. test_imports.py  2. rename test_broker/ => tests/

---
+++ tests/test_imports.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import importlib
+
+from volkanic.introspect import find_all_plain_modules
+
+from joker.broker.environ import GlobalInterface
+
+gi = GlobalInterface()
+
+dotpath_prefixes = [
+    'joker.broker.',
+    'tests.',
+]
+
+
+def _check_prefix(path):
+    for prefix in dotpath_prefixes:
+        if path.startswith(prefix):
+            return True
+    return False
+
+
+def test_module_imports():
+    pdir = gi.under_project_dir()
+    for dotpath in find_all_plain_modules(pdir):
+        if _check_prefix(dotpath):
+            print('importing', dotpath)
+            importlib.import_module(dotpath)
+
+
+if __name__ == '__main__':
+    test_module_imports()
